aeaaefe9dc2be474c6620aade2b9e91d56aada07
brody-code-designer <80767371+Brody-code-designer@users.noreply.github.com>
2022-09-25 09:12:07 -0600

    added form for mosnter slayer clothing

--- public-html/monster-slayer-clothing/src/setupProxy.js
+++ public-html/monster-slayer-clothing/src/setupProxy.js
@@ -1,10 +1,10 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-    app.use(createProxyMiddleware('/apis', {
+    app.use(createProxyMiddleware('/apis/', {
         logLevel: 'debug',
         //Change this to your backend, e.g. http://123.432.653.21:8080
-        target: "http://143.198.157.97:8080",
+        target: "http://localhost:4200",
         changeOrigin: true,
         secure: true,
     }));
