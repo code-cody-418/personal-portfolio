160ab7a99a54213d2ab243a08c285c52a455a624
brody-code-designer <80767371+Brody-code-designer@users.noreply.github.com>
2022-11-04 20:33:05 -0600

    added instanced rocks

--- public-html/stars-floating/src/Experience/Utils/KeyBoard.js
+++ public-html/stars-floating/src/Experience/Utils/KeyBoard.js
@@ -1,5 +1,5 @@
 export default class KeyBoard {
     constructor(){
-        console.log('Keys called');
+        // console.log('Keys called');
     }
 }
--- public-html/stars-floating/src/Experience/World/Environment/Rocks/Rocks-01.js
+++ public-html/stars-floating/src/Experience/World/Environment/Rocks/Rocks-01.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three'
+import * as THREE from "three"
 import Experience from "../../../Experience";
 
 export default class Rock01 {
@@ -9,15 +9,34 @@ export default class Rock01 {
 
         this.resource = this.resources.items.rock01
 
+        this.count = 10
+
         this.setModel()
+        this.setPosition()
     }
 
     setModel() {
         this.model = this.resource.scene
-        this.model.scale.set(5, 5, 5)
-        this.model.position.set(5, 0, 5)
-        console.log(this.model.children);    
+        // this.model.scale.set(5, 5, 5)
+        // this.model.position.set(5, 0, 5)   
+        this.geometry = this.model.children[0].geometry
+        this.material = this.model.children[0].material 
 
-        this.scene.add(this.model)
+        this.instancedMesh = new THREE.InstancedMesh(this.geometry, this.material, this.count)
+
+        this.scene.add(this.instancedMesh)
+    }
+
+    setPosition() {
+        this.dummy = new THREE.Object3D()
+        
+        for (let i = 0; i < this.count; i++) {
+            this.dummy.position.set(i, 0, i)
+            this.dummy.updateMatrix()
+            
+            this.instancedMesh.setMatrixAt(i, this.dummy.matrix)
+
+            this.instancedMesh.needsUpdate = true
+        }
     }
 }
