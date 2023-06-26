9446e6df5420c0f7ea4caa13d6a67ce1a059b591
brody-code-designer <80767371+Brody-code-designer@users.noreply.github.com>
2023-06-25 21:52:04 -0600

    added logos to connect

--- public-html/personal-portfolio/src/ui/Home.js
+++ public-html/personal-portfolio/src/ui/Home.js
@@ -1,17 +1,26 @@
 import React from "react";
-import {ThreeDScene} from "./scene/ThreeDScene";
+import { ThreeDScene } from "./scene/ThreeDScene";
 import './scene/sceneStyle.css'
 
 //if xr is NOT on load css to hide xr button
 const xr = navigator.xr;
 
 if (xr === undefined || null) {
-    import ('../ui/scene/customVRButtonSettings.css')
+    import('../ui/scene/customVRButtonSettings.css')
 }
 export const Home = () => {
     return (
         <>
             <ThreeDScene />
+            {/* <h1 className="connect">Hello</h1> */}
+            <div className="connect-logos">
+                <a href="https://www.linkedin.com/in/code-cody" target={"blank"}>
+                    <img src="linked-in-logo-03.png" alt="LinkedIn Logo" className="connect-logo" />
+                </a>
+                <a href="https://github.com/code-cody-418" target={"blank"}>
+                    <img src="github-icon.png" alt="Github Logo" className="connect-logo" />
+                </a>
+            </div>
         </>
     )
 }
