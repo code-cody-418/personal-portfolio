3fe3362c49072ccaa1a263296ca59e8efe66ffa6
brody-code-designer <80767371+Brody-code-designer@users.noreply.github.com>
2023-07-02 18:33:29 -0600

    added array to map each componenent

--- public-html/personal-portfolio/src/constructor/ModalInfo.js
+++
@@ -1,8 +0,0 @@
-//Object Constructor that sets values for the modal for each Text Object
-export class ModalInfo {
-    constructor() {
-        this.title = null
-        this.subTitle = null
-        this.description = null
-    }
-}
--- public-html/personal-portfolio/src/ui/scene/3d-text/SkillsListText.js
+++ public-html/personal-portfolio/src/ui/scene/3d-text/SkillsListText.js
@@ -3,9 +3,10 @@ import { useResponsive } from "../../../customHooks/useResponsive";
 import { Responsive } from "../../../constructor/Responsive";
 import ReactLogo from "../skills-icons/React_logo";
 import { PortfolioText } from "./PortfolioText";
-import { ModalInfo } from "../../../constructor/ModalInfo";
 
 export const SkillsListText = () => {
+    let yStart = 6 //Value that is the first y coordinate for the map
+
     //A constructor that sets the sizes and position of text for different viewports
     const responsiveData = new Responsive();
     responsiveData.desktopSize = 1.5;
@@ -21,110 +22,41 @@ export const SkillsListText = () => {
     //Passes the Values to a hook that resets the size and position as the viewport changes
     const { size, positionX, positionY, positionZ } = useResponsive(responsiveData);
 
-    //Object Constructor that sets values for the modal for each Text Object
-    const etlInfo = new ModalInfo()
-    etlInfo.title = "Extract, Transform, and Load"
-    etlInfo.subTitle = "Preparing Data for New Systems"
-    etlInfo.description = "Navajo Nation millions of Documents with meta-data transferred. Gesture thousand of customer data transferred. CNM assistant instructor data prepared for student CapStones"
-
-    const dataInt = new ModalInfo()
-    dataInt.title = "Data Integration"
-    dataInt.subTitle = "Synchronizing Data"
-    dataInt.description = "Gesture synchronized data between two Salesforce and Google Firebase"
+    // Array of items that are displayed in list fashion under the Section title
+    // To add new item simple create a new object with the required keys and values
+    const allItems = [
+        {
+            title: "ETL",
+            subTitle: "Preparing Data for New Systems",
+            description: "Navajo Nation millions of Documents with meta-data transferred. Gesture thousand of customer data transferred. CNM assistant instructor data prepared for student CapStones"
+        },
+        {
+            title: "Data Integration",
+            subTitle: "Synchronizing Data",
+            description: "Gesture synchronized data between two Salesforce and Google Firebase"
+        }
+    ]
 
     return (
         <>
             <group //this group moves the whole list
                 position={[positionX, positionY, positionZ]}
             >
-                < PortfolioText
-                    text={"ETL"}
-                    xPosition={20}
-                    yPosition={4} //separate each new item in list by y-2
-                    zPosition={0}
-                    yRotation={-1.570796}
-                    size={size}
-                    modalInfo={etlInfo}
-                />
-
-                < PortfolioText
-                    text={"Data Integration"}
-                    xPosition={20}
-                    yPosition={2} //separate each new item in list by y-2
-                    zPosition={0}
-                    yRotation={-1.570796}
-                    size={size}
-                    modalInfo={dataInt}
-                />
-                {/* <group
-                    position={[20, 4, 0]} //separate each new item in list by y-2
-                    rotation={[0, -1.570796, 0]}
-                >
-                    <FontConfig
-                        text="React"
-                        fontType="/Sunmore-Slant-Free-Regular.json"
-                        size={size}
-                        texture={loadedTexture}
-                    />
-                </group>
-                <group
-                    position={[20, 2, 0]}
-                    rotation={[0, -1.570796, 0]}
-                >
-                    <FontConfig
-                        text="Express"
-                        fontType="/Sunmore-Slant-Free-Regular.json"
-                        size={size}
-                        texture={loadedTexture}
-                    />
-                </group>
-                <group
-
-                    position={[20, 0, 0]}
-                    rotation={[0, -1.570796, 0]}
-                >
-                    <FontConfig
-                        text="Redux"
-                        fontType="/Sunmore-Slant-Free-Regular.json"
-                        size={size}
-                        texture={loadedTexture}
-                    />
-                </group>
-                <group
-                    position={[20, -2, 0]}
-                    rotation={[0, -1.570796, 0]}
-                >
-                    <FontConfig
-                        text="Docker"
-                        fontType="/Sunmore-Slant-Free-Regular.json"
-                        size={size}
-                        texture={loadedTexture}
-                    />
-                </group>
-                <group
-
-                    position={[20, -4, 0]}
-                    rotation={[0, -1.570796, 0]}
-                >
-                    <FontConfig
-                        text="Bootstrap"
-                        fontType="/Sunmore-Slant-Free-Regular.json"
-                        size={size}
-                        texture={loadedTexture}
-                    />
-                </group>
-                <group
-
-                    position={[20, -6, 0]}
-                    rotation={[0, -1.570796, 0]}
-                >
-                    <FontConfig
-                        text="GitHub"
-                        fontType="/Sunmore-Slant-Free-Regular.json"
-                        size={size}
-                        texture={loadedTexture}
-                    />
-                </group> */}
+                {allItems.map((item, index) => { //each item gets rendered into a component
+                    yStart = yStart - 2 // Moves y cordinate down two each item to create list
+                    return (
+                        < PortfolioText
+                            key={index}
+                            text={item.title}
+                            xPosition={20}
+                            yPosition={yStart} //separate each new item in list by y-2
+                            zPosition={0}
+                            yRotation={-1.570796}
+                            size={size}
+                            modalInfo={item}
+                        />
+                    )
+                })}
             </group>
             {/* <SkillsIcons
                     // position={[0, 0, 0]}
