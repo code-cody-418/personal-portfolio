564caebd27c7de2f965aa708965b10fc72ae0f9d
brody-code-designer <80767371+Brody-code-designer@users.noreply.github.com>
2023-07-07 21:54:28 -0600

    added new icon to follow and new favicon

--- public-html/personal-portfolio/src/ui/Home.js
+++ public-html/personal-portfolio/src/ui/Home.js
@@ -13,6 +13,9 @@ export const Home = () => {
         <>
             <ThreeDScene />
             <div className="connect-logos">
+                <a href="https://github.com/code-cody-418/personal-vm-nginx/tree/main/public-html/personal-portfolio" target={"blank"}>
+                    <img src="code-icon-10.png" alt="Code Logo" className="connect-logo" />
+                </a>
                 <a href="https://www.linkedin.com/in/code-cody" target={"blank"}>
                     <img src="linked-in-logo-03.png" alt="LinkedIn Logo" className="connect-logo" />
                 </a>
