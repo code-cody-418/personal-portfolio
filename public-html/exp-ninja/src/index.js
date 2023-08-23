6dfe5a50473f8eebfd7c2164b8212ee7cc6df6c7
code-cody-418 <code_slayer1@protonmail.com>
2023-08-23 07:01:47 -0600

    setup

--- public-html/exp-ninja/src/index.js
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom'
-import {App} from "./ui/App"
-import 'bootstrap/dist/css/bootstrap.css';
-import {store} from "./store";
-
-
-ReactDOM.render(App(store), document.querySelector('#root'));
-
--- public-html/stars-floating/documentation/attribute.md
+++
@@ -1,8 +0,0 @@
-# Asset Attribute
-
-## Dragon
-`/models/dragon/dragon.gltf`
-- <https://quaternius.com/packs/animatedmonster.html>
-
-## LandScape stuff
-- <https://kenney.nl/assets/survival-kit>
