ab6a0a1e2bac5bdccd82517aee9047b963437c95
code-cody-418 <code_slayer1@protonmail.com>
2023-09-08 22:58:03 -0600

    added nn seal logo

--- src/ui/modal/ThreeLogo.js
+++ src/ui/modal/ThreeLogo.js
@@ -1,11 +1,27 @@
+import { Canvas } from "@react-three/fiber";
 import React from "react";
 import GithubLogo from "../scene/skills-icons/Github_logo";
+import { NNSeal } from "../scene/skills-icons/Nn-seal";
 import ReactLogo from "../scene/skills-icons/React_logo";
 
-export const ThreeLogo = ({threeLogo}) => {
-    if(threeLogo === "GithubLogo"){
-        return  <GithubLogo />
-    } else if (threeLogo === "ReactLogo"){
-        return  <ReactLogo />
+//Check to see type of logo to be rendered
+const Logo = ({ threeLogo }) => {
+    if (threeLogo === "GithubLogo") {
+        return <GithubLogo />
+    } else if (threeLogo === "ReactLogo") {
+        return <ReactLogo />
+    } else if (threeLogo === "NNSeal") {
+        return <NNSeal />
     }
+}
+
+export const ThreeLogo = ({ threeLogo }) => {
+    return (
+        <>
+            <Canvas>
+                <directionalLight position={[0, 0, 10]} intensity={1} />
+                <Logo threeLogo={threeLogo} />
+            </Canvas>
+        </>
+    )
 }
--- src/ui/scene/3d-text/EmployersListText.js
+++ src/ui/scene/3d-text/EmployersListText.js
@@ -26,7 +26,8 @@ export const EmployersListText = () => {
             title: "Navajo Nation",
             subTitle: "Programmer Analyst",
             description: "With the Navajo Nation I rebuilt a website, conducted a data transfer, built a Help Desk application, and maintained VM infrastructure. WIth the website, I used modern styling techniques to create a mobile responsive site using the Bootstrap library. Also, old hard coded data was converted to display dynamically and a search algorithm was added that could filter job data. On the other hand, I successfully transferred over 2 million documents from an old database to a new one. This was done using the extraction, transformation, and loading methodology. The data was extracted into CSV format, transformed using JavaScript, and then loaded using the database CMS gui.",
-            img: null
+            img: null,
+            threeLogo: "NNSeal"
         },
         {
             title: "Gesture",
