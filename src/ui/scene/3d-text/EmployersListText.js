f4a5c9d6074af4e73631851f024cf5171b4273cf
code-cody-418 <code_slayer1@protonmail.com>
2023-09-18 06:46:30 -0600

    added logos for experince

--- src/ui/scene/3d-text/EmployersListText.js
+++ src/ui/scene/3d-text/EmployersListText.js
@@ -25,7 +25,7 @@ export const EmployersListText = () => {
         {
             title: "Navajo Nation",
             subTitle: "Programmer Analyst",
-            description: "With the Navajo Nation I rebuilt a website, conducted a data transfer, built a Help Desk application, and maintained VM infrastructure. WIth the website, I used modern styling techniques to create a mobile responsive site using the Bootstrap library. Also, old hard coded data was converted to display dynamically and a search algorithm was added that could filter job data. On the other hand, I successfully transferred over 2 million documents from an old database to a new one. This was done using the extraction, transformation, and loading methodology. The data was extracted into CSV format, transformed using JavaScript, and then loaded using the database CMS gui.",
+            description: "With the Navajo Nation I conducted a data transfer, rebuilt the website, built a Help Desk application, and maintained VM infrastructure. First off, I saved the department 133% of my yearly salary by successfully transferring over 2 million documents to a new database in just a couple months. The data was extracted into CSV, transformed using JavaScript, and then loaded using the database GUI. With the website I used modern styling techniques to create a mobile responsive site by using the Bootstrap library. In addition, hard coded job data was converted to display data dynamically. This automation saved one employee four hours a day. Also, a search bar was added that could filter jobs allowing a better user experience. Lastly, the Help Desk application integrated the Zoom REST API to programmatically send ticket updates to the entire team.",
             img: null,
             threeLogo: "NNSeal"
         },
@@ -33,19 +33,22 @@ export const EmployersListText = () => {
             title: "Gesture",
             subTitle: "Intern Backend Engineer",
             description: "At Gesture, I built ETL pipelines for data syncing between our databases and the Salesforce       CRM. To do this I had to authenticate requests to Salesforce using JWT tokens and certificates. Then I created an express server, utilizing Google Firebase functions, with a trigger that would sync our new data to Salesforce. Lastly, webhooks were utilized, using Flow and Outbound Messages, to update our database directly from Salesforce.",
-            img: null
+            img: "/experience/Gesture-Logo-Wordmark.webp",
+            roundImg: false
         },
         {
             title: "CNM",
             subTitle: "Assistant Instructor",
             description: "At this bootcamp I taught students how to build Web Applications using a modified MERN Stack. Other tools shown to students included git and Docker. With this stack and tools I helped students successfully create full blown web apps. Students were also taught how to build RESTful api’s, as well as how to develop data-downloader’s to store external API data.",
-            img: null
+            img: "/experience/CNM_Ingenuity_WhiteText.png",
+            roundImg: false
         },
         {
             title: "Freelance",
             subTitle: "Web Developer",
             description: "My job was building websites to meet the clients needs. Clients required 3d websites, which were created using threeJS. These sites consisted of walking characters and environments. One project also required multiple users to be in one room, so socket.io was utilized to create a server with UDP sockets that allowed multiple users to wander around a room together.",
-            img: null
+            img: "/experience/upwork-logo.svg",
+            roundImg: false
         },
     ]
 
