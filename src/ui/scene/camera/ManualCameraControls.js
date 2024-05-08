79ad4d19582f107d67a1ad03f0a1523822e689e9
code-cody-418 <code_slayer1@protonmail.com>
2024-05-07 20:49:40 -0600

    cleaned up and added arrows for controls

--- src/ui/scene/camera/ManualCameraControls.js
+++ src/ui/scene/camera/ManualCameraControls.js
@@ -1,4 +1,3 @@
-import { Button } from "react-bootstrap";
 import { useModalStore } from "../../utils/store";
 
 export const ManualCameraControls = () => {
@@ -19,35 +18,59 @@ export const ManualCameraControls = () => {
   );
 
   const handleBackwards = () => {
-    enableManualControls()
-    setManualControlDirectionBackward()
-  }
+    enableManualControls();
+    setManualControlDirectionBackward();
+  };
 
   const handleForwards = () => {
-    enableManualControls()
-    setManualControlDirectionForward()
-  }
+    enableManualControls();
+    setManualControlDirectionForward();
+  };
 
   return (
     <>
       <div className="manualControlsPosition">
-        <Button
-          variant="primary"
+        <span
           className="manualControlsButton"
           onMouseEnter={handleBackwards}
           onMouseLeave={disableManualControls}
         >
-          backward
-        </Button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            fill="#c5cfd5"
+            class="bi bi-arrow-left-circle"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"
+            />
+          </svg>
+        </span>
 
-        <Button
-          variant="secondary"
+        <span style={{ margin: "20px" }}></span>
+
+        <span
           className="manualControlsButton"
           onMouseEnter={handleForwards}
           onMouseLeave={disableManualControls}
         >
-          forward
-        </Button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            fill="#c5cfd5"
+            class="bi bi-arrow-right-circle"
+            viewBox="0 0 16 16"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"
+            />
+          </svg>
+        </span>
       </div>
     </>
   );
