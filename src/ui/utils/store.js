c5c5af5ede876db5f0370b01b424bc3142ac198e
code-cody-418 <code_slayer1@protonmail.com>
2024-05-13 22:10:05 -0600

    added state for analytics

--- src/ui/utils/store.js
+++ src/ui/utils/store.js
@@ -1,30 +1,45 @@
-import { create } from 'zustand'
+import { create } from "zustand";
 
 export const useStore = create((set) => ({
-    //set the modal to turn on and off
-    modalState: false,
-    showModal: () => set(() => ({ modalState: true })),
-    closeModal: () => set(() => ({ modalState: false })),
+  //set the modal to turn on and off
+  modalState: false,
+  showModal: () => set(() => ({ modalState: true })),
+  closeModal: () => set(() => ({ modalState: false })),
 
-    //set allModalInfo
-    allModalInfo: null,
-    setAllModalInfo: (allModalInfo) => set(() => ({ allModalInfo: allModalInfo})),
+  //set allModalInfo
+  allModalInfo: null,
+  setAllModalInfo: (allModalInfo) =>
+    set(() => ({ allModalInfo: allModalInfo })),
 
-    //set allListItems
-    allListItems: null,
-    setAllListItems: (allListItems) => set(() => ({allListItems: allListItems})),
+  //set allListItems
+  allListItems: null,
+  setAllListItems: (allListItems) =>
+    set(() => ({ allListItems: allListItems })),
 
-    //setCurrentItemIndex
-    currentItemIndex: null,
-    setCurrentItemIndex: (currentItemIndex) => set(() => ({currentItemIndex: currentItemIndex})),
+  //setCurrentItemIndex
+  currentItemIndex: null,
+  setCurrentItemIndex: (currentItemIndex) =>
+    set(() => ({ currentItemIndex: currentItemIndex })),
 
-    //set manual controls
-    manualControlState: false,
-    enableManualControls: () => set(() => ({manualControlState: true})),
-    disableManualControls: () => set(() => ({manualControlState: false})),
+  //set manual controls
+  manualControlState: false,
+  enableManualControls: () => set(() => ({ manualControlState: true })),
+  disableManualControls: () => set(() => ({ manualControlState: false })),
 
-    //set manual control direction
-    manualControlDirection: 5,
-    setManualControlDirectionForward: () => set( () => ({manualControlDirection: 5})),
-    setManualControlDirectionBackward: () => set( () => ({manualControlDirection: -5}))
-}))
+  //set manual control direction
+  manualControlDirection: 5,
+  setManualControlDirectionForward: () =>
+    set(() => ({ manualControlDirection: 5 })),
+  setManualControlDirectionBackward: () =>
+    set(() => ({ manualControlDirection: -5 })),
+
+  // set analytics clicks
+  sessionClicks: 0,
+  setSessionClicks: (watchClicks) =>
+    set(() => ({sessionClicks: watchClicks})),
+
+  // set analytics time
+  sessionTime: 0,
+  setSessionTime: (secondsPassed) => 
+    set(() => ({sessionTime: secondsPassed}))
+}));
