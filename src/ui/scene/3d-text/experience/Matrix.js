0166397102cfe52425b143397fa95308466a7501
code-cody-418 <code_slayer1@protonmail.com>
2025-08-10 20:57:03 -0700

    removed signal rgb api

--- src/ui/scene/3d-text/experience/Matrix.js
+++ src/ui/scene/3d-text/experience/Matrix.js
@@ -7,8 +7,6 @@ Files: matrix.glb [990.14KB] > C:\code\personal-portfolio\public\matrix\matrix-t
 import React, { useEffect, useState } from "react";
 import { useGLTF, useAnimations } from "@react-three/drei";
 import { useStore } from "../../../utils/store";
-import { setEffectStars } from "../../utils/signalRGB/SetEffectStars";
-import { setEffectMatrix } from "../../utils/signalRGB/SetEffectMatrix";
 
 const matrixPartialHeightStart = -90;
 const matrixPartialHeightEnd = -208;
@@ -27,10 +25,8 @@ export function Matrix({ animationSpeed }) {
       cameraHeight >= matrixPartialHeightStart ||
       cameraHeight <= matrixPartialHeightEnd
     ) {
-      setEffectStars()
       setShowPartialMatrix(false);
     } else {
-      setEffectMatrix()
       setShowPartialMatrix(true);
     }
   }, [cameraHeight]);
